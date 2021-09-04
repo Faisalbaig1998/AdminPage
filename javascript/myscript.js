abeadf86b0cd00761c9a62ac0a968c9679446a6c
Faisal Baig <mirzaf317@gmail.com>
2021-09-04 22:55:10 +0530

    added more input fields

---
+++ javascript/myscript.js
@@ -0,0 +1,6 @@
+const expand = () => {
+  console.log("you are in expand");
+  document.getElementById("login-details").style.width = "0px";
+  document.getElementById("login-details").style.height = "0px";
+  document.getElementById("login-details").style.transition = "all 0.2s";
+};
