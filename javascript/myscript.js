2b4335c42d3f9fd44feb23179ca2f77c75bd7b54
Faisal Baig <mirzaf317@gmail.com>
2021-09-05 20:09:11 +0530

    added fonts

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,6 +1,14 @@
 const expand = () => {
   console.log("you are in expand");
-  document.getElementById("login-details").style.width = "0px";
-  document.getElementById("login-details").style.height = "0px";
-  document.getElementById("login-details").style.transition = "all 0.2s";
+  var value = getComputedStyle(document.getElementById("login-details"));
+  console.log(value.backgroundColor);
+  if (value.width != "250px") {
+    document.getElementById("login-details").style.width = "250px";
+    document.getElementById("login-details").style.height = "auto";
+    document.getElementById("login-details").style.transition = "all 0.4s";
+  } else {
+    document.getElementById("login-details").style.width = "0px";
+    document.getElementById("login-details").style.height = "0px";
+    document.getElementById("login-details").style.transition = "all 0.4s";
+  }
 };
