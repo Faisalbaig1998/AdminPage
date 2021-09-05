b37e0c9473d0f8bce3fad6e779f1e94522a49a39
Faisal Baig <mirzaf317@gmail.com>
2021-09-06 01:02:31 +0530

    added to the sidebar

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,14 +1,14 @@
 const expand = () => {
   console.log("you are in expand");
   var value = getComputedStyle(document.getElementById("login-details"));
-  console.log(value.backgroundColor);
-  if (value.width != "250px") {
-    document.getElementById("login-details").style.width = "250px";
-    document.getElementById("login-details").style.height = "auto";
+  let px = parseInt(value.height);
+  console.log(px);
+  console.log(value.height);
+  if (value.display !== "none") {
+    document.getElementById("login-details").style.display = "none";
     document.getElementById("login-details").style.transition = "all 0.4s";
   } else {
-    document.getElementById("login-details").style.width = "0px";
-    document.getElementById("login-details").style.height = "0px";
+    document.getElementById("login-details").style.display = "block";
     document.getElementById("login-details").style.transition = "all 0.4s";
   }
 };
