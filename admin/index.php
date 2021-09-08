ca170a2da19cb5b7e0add3d03235304df3267725
Faisal Baig <mirzaf317@gmail.com>
2021-09-08 23:30:19 +0530

    changes made in experimental branch

--- admin/index.php
+++ admin/index.php
@@ -1,2 +1,15 @@
 <?php include('header.inc.php') ?>
+<!-- Main Starts here -->
+<div id="main">
+    <form action="Action.php" method="POST">
+        <h2>Contact Us</h2>
+        <input type="text" name="name" placeholder="Name" />
+        <input type="email" name="email" placeholder="E-mail" />
+        <input type="text" name="number" placeholder="4895495945" autocomplete="mobile" required />
+        <textarea name="messagage" id="message"> </textarea>
+        <button type="submit" name="submit">Submit</button>
+        <!-- <input type="submit" name="submit" /> -->
+    </form>
+</div>
+<!-- Main Ends Here -->
 <?php include('footer.inc.php') ?>
