47abbdc56205fd6fb008723eca65d3add9caf550
Faisal Baig <mirzaf317@gmail.com>
2021-10-17 00:42:13 +0530

    added some other functions

--- contact.php
+++ contact.php
@@ -1,56 +1,49 @@
-    <!DOCTYPE html>
-    <html lang="en">
+<!doctype html>
+<html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <title>Contact Us</title>
-    </head>
-    <style>
-        *{
-            margin:0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-    </style>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <body>
-        <div class="container-fluid m-0">
-            <h1 class="text-center bg-dark text-light">Contact Us</h1>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Air Borne Shoe</title>
+</head>
+
+<body>
+
+    <?php include 'partials/header.inc.php'; ?>
+    <div class="container-fluid">
+        <h1 class="text-center bg-dark text-light">Contact Us</h1>
+    </div>
+    <div class="container" style="min-height: 333px;">
+        <div class="row g-3">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+            </div>
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+            </div>
         </div>
-        <div class="container col-6
-         ">
-            <form class="row g-3">
-                <div class="col-md-6">
-                    <label for="inputfirstname" class="form-label">First name</label>
-                    <input type="text" name="fname" class="form-control" id="inputfirstname">
-                </div>
-                <div class="col-md-6">
-                    <label for="inputlastname" class="form-label">Last name</label>
-                    <input type="text" name="lname" class="form-control" id="inputlastname">
-                </div>
-                <div class="col-md-6">
-                    <label for="inputNumber" class="form-label">Number</label>
-                    <input type="number" class="form-control" id="inputNumber">
-                </div>
-                <div class="col-12">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="gridCheck">
-                        <label class="form-check-label" for="gridCheck">
-                            Check me out
-                        </label>
-                    </div>
-                </div>
-                <div class="col-12">
-                    <button type="submit" class="btn btn-primary">Sign in</button>
-                </div>
-            </form>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-        </script>
+    </div>
+    <?php include 'partials/footer.inc.php'; ?>
 
-    </body>
 
-    </html>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
---
+++ partials/footer.inc.php
@@ -0,0 +1,3 @@
+<div class="container-fluid bg-dark">
+    <h1 class="text-center text-light">Footer</h1>
+</div>
