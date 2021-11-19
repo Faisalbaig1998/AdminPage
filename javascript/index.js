5a1738fc1b0051445b61db9514e762f4b8aeea7c
Faisal Baig <mirzaf317@gmail.com>
2021-11-20 00:21:27 +0530

    added icon and index.js

---
+++ javascript/index.js
@@ -0,0 +1,6 @@
+var images = ['DSC_6442.JPG','DSC_6443.JPG','DSC_6444.JPG','DSC_6445.JPG','DSC_6447.JPG','DSC_6450.JPG'];
+
+const next = (images) => {
+    let element = document.getElementsByClassName('tags')[0];
+    
+}
