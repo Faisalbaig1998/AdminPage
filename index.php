50a1238c92fff597673b4cd9c6a5a56242f25394
Faisal Baig <mirzaf317@gmail.com>
2021-11-20 00:20:48 +0530

    added something

--- index.php
+++ index.php
@@ -5,33 +5,43 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/icons/Air_Borne.png" type="image/x-icon">
     <link rel="stylesheet" href="css/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Courgette&family=Itim&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Castoro&display=swap" rel="stylesheet">
     <title>Air Borne Shoe</title>
 </head>
 
 <body>
     <div>
-    <nav class="navbar" style="position:fixed;">
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="contact.php">Contact Us</a></li>
-        <li><a href="about.php">About</a></li>
-    </ul>
-</nav>
+        <nav class="navbar" style="position:fixed;">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+                <li><a href="about.php">About</a></li>
+            </ul>
+        </nav>
         <img src="https://source.unsplash.com/1600x900/?shoes,boots" alt="" class="header">
     </div>
     <!-- <?php include 'partials/header.inc.php'; ?> -->
     <div class="container">
         <div class="card">
             <div class="tags" style="display:flex;justify-content:center;align-items:center;">
-                <span><b>&#8249</b></span>
+                <span><b>&#10094</b></span>
                 <img src="images/002/DSC_6343.JPG" alt="">
-                <span><b>&#8250</b></span>
+                <span><b>&#10095</b></span>
+            </div>
+            <div class="title">
+                <p>Air Borne Eshu shoes</p>
             </div>
             <div class="social">
                 <span class="flipkart"><a href="https://www.flipkart.com/"><img src="images/icons/flipkart.png"
@@ -43,99 +53,16 @@
                             src="images/icons/whatsapp.png" style="width:60px;height:60px;" alt=""></a></span>
             </div>
         </div>
-        
-        
-        
-        
-        
-        <div class="card">
-            <div class="tags" style="display:flex;justify-content:center;align-items:center;">
-                <span><b>&#8249</b></span>
-                <img src="images/002/DSC_6343.JPG" alt="">
-                <span><b>&#8250</b></span>
-            </div>
-            <div class="social">
-                <span class="flipkart"><a href="https://www.flipkart.com/"><img src="images/icons/flipkart.png"
-                            style="width:60px;height:60px;" alt=""></a></span>
-                <span class="amazon"><a href="https://www.amazon.in/"><img src="images/icons/Amazon.jpg"
-                            style="width:60px;height:60px;" alt=""></a></span>
-                <span class="whatsapp"><a
-                        href="https://wa.me/+917251863460?text=I'm%20interested%20in%20your%20car%20for%20sale"><img
-                            src="images/icons/whatsapp.png" style="width:60px;height:60px;" alt=""></a></span>
-            </div>
-        </div>
-        <div class="card">
-            <div class="tags" style="display:flex;justify-content:center;align-items:center;">
-                <span><b>&#8249</b></span>
-                <img src="images/002/DSC_6343.JPG" alt="">
-                <span><b>&#8250</b></span>
-            </div>
-            <div class="social">
-                <span class="flipkart"><a href="https://www.flipkart.com/"><img src="images/icons/flipkart.png"
-                            style="width:60px;height:60px;" alt=""></a></span>
-                <span class="amazon"><a href="https://www.amazon.in/"><img src="images/icons/Amazon.jpg"
-                            style="width:60px;height:60px;" alt=""></a></span>
-                <span class="whatsapp"><a
-                        href="https://wa.me/+917251863460?text=I'm%20interested%20in%20your%20car%20for%20sale"><img
-                            src="images/icons/whatsapp.png" style="width:60px;height:60px;" alt=""></a></span>
-            </div>
-        </div>
-        <div class="card">
-            <div class="tags" style="display:flex;justify-content:center;align-items:center;">
-                <span><b>&#8249</b></span>
-                <img src="images/002/DSC_6343.JPG" alt="">
-                <span><b>&#8250</b></span>
-            </div>
-            <div class="social">
-                <span class="flipkart"><a href="https://www.flipkart.com/"><img src="images/icons/flipkart.png"
-                            style="width:60px;height:60px;" alt=""></a></span>
-                <span class="amazon"><a href="https://www.amazon.in/"><img src="images/icons/Amazon.jpg"
-                            style="width:60px;height:60px;" alt=""></a></span>
-                <span class="whatsapp"><a
-                        href="https://wa.me/+917251863460?text=I'm%20interested%20in%20your%20car%20for%20sale"><img
-                            src="images/icons/whatsapp.png" style="width:60px;height:60px;" alt=""></a></span>
-            </div>
-        </div>
-        <div class="card">
-            <div class="tags" style="display:flex;justify-content:center;align-items:center;">
-                <span><b>&#8249</b></span>
-                <img src="images/002/DSC_6343.JPG" alt="">
-                <span><b>&#8250</b></span>
-            </div>
-            <div class="social">
-                <span class="flipkart"><a href="https://www.flipkart.com/"><img src="images/icons/flipkart.png"
-                            style="width:60px;height:60px;" alt=""></a></span>
-                <span class="amazon"><a href="https://www.amazon.in/"><img src="images/icons/Amazon.jpg"
-                            style="width:60px;height:60px;" alt=""></a></span>
-                <span class="whatsapp"><a
-                        href="https://wa.me/+917251863460?text=I'm%20interested%20in%20your%20car%20for%20sale"><img
-                            src="images/icons/whatsapp.png" style="width:60px;height:60px;" alt=""></a></span>
-            </div>
-        </div>
-        <div class="card">
-            <div class="tags" style="display:flex;justify-content:center;align-items:center;">
-                <span><b>&#8249</b></span>
-                <img src="images/002/DSC_6343.JPG" alt="">
-                <span><b>&#8250</b></span>
-            </div>
-            <div class="social">
-                <span class="flipkart"><a href="https://www.flipkart.com/"><img src="images/icons/flipkart.png"
-                            style="width:60px;height:60px;" alt=""></a></span>
-                <span class="amazon"><a href="https://www.amazon.in/"><img src="images/icons/Amazon.jpg"
-                            style="width:60px;height:60px;" alt=""></a></span>
-                <span class="whatsapp"><a
-                        href="https://wa.me/+917251863460?text=I'm%20interested%20in%20your%20car%20for%20sale"><img
-                            src="images/icons/whatsapp.png" style="width:60px;height:60px;" alt=""></a></span>
-            </div>
-        </div>
+
+
 
 
 
 
 
     </div>
-    <?php include 'partials/footer.inc.php';?>
-
+    <?php include 'partials/footer.inc.php'; ?>
+    <script src="javascript/index.js"></script>
 </body>
 
 </html>
